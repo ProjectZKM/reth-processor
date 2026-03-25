cc6af1daee39c14a5bfe7456e1069595d0b6bce0
Jingping Yin <yinjingping2022@gmail.com>
2026-03-25 19:35:49 +0800

    feat: continuously processing blocks (#13)

---
+++ bin/continuous/src/cli.rs
@@ -0,0 +1,103 @@
+use std::{fs, path::PathBuf};
+
+use alloy_chains::Chain;
+use alloy_provider::{network::AnyNetwork, Provider, RootProvider};
+use clap::Parser;
+use host_executor::Config;
+use primitives::genesis::Genesis;
+use url::Url;
+use zkm_sdk::ZKMProofKind;
+
+/// The arguments for the cli.
+#[derive(Debug, Clone, Parser)]
+pub struct Args {
+    /// The block number of the block to execute.
+    #[clap(long, env, default_value_t = 1)]
+    pub block_number: u64,
+
+    /// The rpc url used to fetch data about the block. If not provided, will use the
+    /// RPC_{chain_id} env var.
+    #[clap(long)]
+    pub rpc_url: Option<Url>,
+
+    /// The chain ID. If not provided, requires the rpc_url argument to be provided.
+    #[clap(long, env)]
+    pub chain_id: Option<u64>,
+
+    /// The path to the genesis json file to use for the execution.
+    #[clap(long, env)]
+    pub genesis_path: Option<PathBuf>,
+
+    /// The maximum number of concurrent executions.
+    #[clap(long, env, default_value_t = 1)]
+    pub max_concurrent_executions: usize,
+
+    /// Retry count on failed execution.
+    #[clap(long, env, default_value_t = 1)]
+    pub execution_retries: usize,
+
+    /// Whether to generate a proof or just execute the block.
+    #[clap(long, env, default_value_t = false)]
+    pub prove: bool,
+
+    /// PagerDuty integration key.
+    #[clap(long, env)]
+    pub pager_duty_integration_key: Option<String>,
+}
+
+impl Args {
+    pub async fn as_config(&self) -> eyre::Result<Config> {
+        // We don't need RPC when using cache with known chain ID, so we leave it as `Option<Url>`
+        // here and decide on whether to panic later.
+        //
+        // On the other hand chain ID is always needed.
+        let (rpc_url, chain_id) = match (self.rpc_url.clone(), self.chain_id) {
+            (Some(rpc_url), Some(chain_id)) => (Some(rpc_url), chain_id),
+            (None, Some(chain_id)) => {
+                match std::env::var(format!("RPC_{chain_id}")) {
+                    Ok(rpc_env_var) => {
+                        // We don't always need it but if the value exists it has to be valid.
+                        (Some(Url::parse(rpc_env_var.as_str())?), chain_id)
+                    }
+                    Err(_) => {
+                        // Not having RPC is okay because we know chain ID.
+                        (None, chain_id)
+                    }
+                }
+            }
+            (Some(rpc_url), None) => {
+                // We can find out about chain ID from RPC.
+                let provider = RootProvider::<AnyNetwork>::new_http(rpc_url.clone());
+
+                (Some(rpc_url), provider.get_chain_id().await?)
+            }
+            (None, None) => {
+                eyre::bail!("either --rpc-url or --chain-id must be used")
+            }
+        };
+
+        let genesis = if let Some(genesis_path) = &self.genesis_path {
+            let genesis_json = fs::read_to_string(genesis_path)
+                .map_err(|err| eyre::eyre!("Failed to read genesis file: {err}"))?;
+
+            Genesis::Custom(genesis_json)
+        } else {
+            chain_id.try_into()?
+        };
+
+        let chain = Chain::from_id(chain_id);
+
+        let config = Config {
+            chain,
+            genesis,
+            rpc_url,
+            cache_dir: None,
+            custom_beneficiary: None,
+            prove_mode: self.prove.then_some(ZKMProofKind::Compressed),
+            opcode_tracking: false,
+            debug_rpc_url: None,
+        };
+
+        Ok(config)
+    }
+}
